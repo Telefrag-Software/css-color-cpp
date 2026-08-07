02e17b5a42f4c068038d3ae0772d33cdf323a82a
Kevin Scroggins <nitro404@gmail.com>
2026-08-07 00:21:12 -0400

    Add CMake package support, fix missing inline declarations

--- io.hpp
+++ io.hpp
@@ -122,21 +122,21 @@ namespace css_colors::details {
 			}
 			return os;
 		}
-		std::ostream& operator << (std::ostream& os, const untyped_color& color) {
+		inline std::ostream& operator << (std::ostream& os, const untyped_color& color) {
 			return print_untyped<false>(os, color);	//as a default
 		}
 		template<class colorspace>
-		std::ostream& operator << (std::ostream& os, const typed_color<colorspace>& color) {
+		inline std::ostream& operator << (std::ostream& os, const typed_color<colorspace>& color) {
 			print_prefix<colorspace>::print(os);
 			print_untyped<is_legacy_v<colorspace>>(os, static_cast<const untyped_color&>(color));
 			os << ")";
 		}
-		void u8_to_hexstr(char result[2], uint8_t d) {
+		inline void u8_to_hexstr(char result[2], uint8_t d) {
 			constexpr char lookup[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
 			result[0] = lookup[d / 16];
 			result[1] = lookup[d % 16];
 		}
-		std::ostream& operator << (std::ostream& os, const color& c) {
+		inline std::ostream& operator << (std::ostream& os, const color& c) {
 			if (!c) {
 				return (os << "[Invalid Color]");
 			}
@@ -197,7 +197,7 @@ namespace css_colors::details {
 			inline constexpr static size_t max_len = std::string_view("color").length();
 		};
 
-		std::istream& operator >> (std::istream& is, color& c) {
+		inline std::istream& operator >> (std::istream& is, color& c) {
 			size_t read_n = 0;
 			std::string cache;
 			cache.reserve(function_name_info<0>::max_len + 1);
